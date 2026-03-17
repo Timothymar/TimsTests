219bf04e71e12c33afc16a3e5b1e8ba5fab20660
Timothy Martin <TJMARTIN7@STUDENT.FULLSAIL.EDU>
2026-03-17 01:13:56 -0400

    Save/Load slots created need to finish after character creation

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//public class MainMenu : MonoBehaviour
+//{
+//    public void NewGame()
+//    {
+//        LevelManager.Instance.LoadNewGame();
+//    }
+
+//}
 public class MainMenu : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject mainButtonsPanel;
+    public GameObject saveSlotPanel;
+
     public void NewGame()
     {
+        mainButtonsPanel.SetActive(false);
+        saveSlotPanel.SetActive(true);
+    }
+
+    public void BackFromSaveSlots()
+    {
+        saveSlotPanel.SetActive(false);
+        mainButtonsPanel.SetActive(true);
+    }
+
+    public void SelectSaveSlot(int slotNumber)
+    {
+        Debug.Log("Selected save slot: " + slotNumber);
+
+        // For now, just start the new game after selecting a slot.
+        // Later we’ll save this slot choice and use it properly.
         LevelManager.Instance.LoadNewGame();
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game clicked");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
